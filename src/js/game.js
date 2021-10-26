74ff4b908c2c557e75463711f192299b52ef7f4d
Jeff Nyman <jeffnyman@gmail.com>
2021-10-26 16:48:03 -0500

    Placement of the log section of the UI.

--- src/js/game.js
+++ src/js/game.js
@@ -2,6 +2,7 @@ console.log("Game Loaded.");
 
 import * as splash from "screen/splash/splash.js";
 import * as map from "screen/map/map.js";
+import * as log from "screen/log.js";
 
 let seed = Date.now();
 console.log("Seed: ", seed);
@@ -11,6 +12,7 @@ function start() {
   console.log("Game Started.");
 
   map.setup(document.querySelector("#map"));
+  log.setup(document.querySelector("#log"));
 }
 
 splash.load(document.querySelector("#game")).then(start);
---
+++ src/js/screen/log.js
@@ -0,0 +1,8 @@
+let element;
+
+export function setup(parent) {
+  element = parent;
+  element.classList.remove("hidden");
+
+  console.log(element);
+}
